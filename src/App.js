33b75630559abccdacbb3e9984c12584ad69ceb7
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-03 20:31:34 -0500

    Styling Numbers and Specials

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ function App() {
 					<Specials />
 					<Numbers />
 				</div>
-				<Operators />
+				<div className="columnTwo">
+					<Operators />
+				</div>
 			</section>
 			<div className="App">
 				{/* STEP 4 - Render your components here and be sure to properly import/export all files */}
